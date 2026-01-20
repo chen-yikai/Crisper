40f8f31b4fc227d56e98a50480b9099adc7eefe5
Yi Kai <eliaschen.dev+chen_yikai@gmail.com>
2026-01-20 23:48:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /app/backend && bun run ./src/index.ts &
 
-ollama-mcp-bridge --config ./mcp-config.json --port 8000 --ollama-url https://ollama.crisper.skills.eliaschen.dev
+ollama-mcp-bridge --config ./mcp-config.json --port 8000 --ollama-url https://ollama.skills.eliaschen.dev
